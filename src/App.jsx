2dfb13c12a1df859fa2c60827c8150c378962d12
Michelle-Watson <alicerockshard@gmail.com>
2024-11-12 21:40:31 -0500

    Added h1 text to show that font renders

--- src/App.jsx
+++ src/App.jsx
@@ -10,21 +10,22 @@ import InventoryEdit from "./pages/InventoryEdit/InventoryEdit";
 import InventoryAdd from "./pages/InventoryAdd/InventoryAdd";
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Warehouses />} />
-				<Route path="warehouses" element={<Warehouses />} />
-				<Route path="warehouses/:id" element={<WarehouseDetails />} />
-				<Route path="warehouses/:id/edit" element={<WarehouseEdit />} />
-				<Route path="warehouses/add" element={<WarehouseAdd />} />
-				<Route path="inventory" element={<Inventory />} />
-				<Route path="inventory/:id" element={<InventoryDetails />} />
-				<Route path="inventory/:id/edit" element={<InventoryEdit />} />
-				<Route path="inventory/add" element={<InventoryAdd />} />
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <h1>Test Font Rendering</h1>
+      <Routes>
+        <Route path="/" element={<Warehouses />} />
+        <Route path="warehouses" element={<Warehouses />} />
+        <Route path="warehouses/:id" element={<WarehouseDetails />} />
+        <Route path="warehouses/:id/edit" element={<WarehouseEdit />} />
+        <Route path="warehouses/add" element={<WarehouseAdd />} />
+        <Route path="inventory" element={<Inventory />} />
+        <Route path="inventory/:id" element={<InventoryDetails />} />
+        <Route path="inventory/:id/edit" element={<InventoryEdit />} />
+        <Route path="inventory/add" element={<InventoryAdd />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
