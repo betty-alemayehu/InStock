a1704041861084f36977ce62e42a111779e6305f
Ife Onuorah <ifeonuorah@gmail.com>
2024-11-15 18:48:28 -0400

    Change routing for warehouse details page

--- src/components/WarehouseRow/WarehouseRow.jsx
+++ src/components/WarehouseRow/WarehouseRow.jsx
@@ -6,80 +6,80 @@ import deleteImage from "/assets/icons/delete_outline-24px.svg";
 import editImage from "/assets/icons/edit-24px.svg";
 
 function WarehouseRow({ warehouse, generateWarehouseItems }) {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+	const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-  };
+	const handleCloseModal = () => {
+		setIsModalOpen(false);
+	};
 
-  const handleDeleteSuccess = () => {
-    generateWarehouseItems();
-  };
+	const handleDeleteSuccess = () => {
+		generateWarehouseItems();
+	};
 
-  return (
-    <ul>
-      <li className="warehouse-row__row">
-        <div
-          className="warehouse-row__col warehouse-row__col--1"
-          data-label="warehouse item"
-        >
-          <Link
-            className="warehouse-row__col--title"
-            to={`/warehouse/${warehouse.id}`}
-          >
-            {warehouse.warehouse_name}
-          </Link>
-        </div>
-        <div
-          className="warehouse-row__col warehouse-row__col--2"
-          data-label="Address"
-        >
-          {warehouse.address}, {warehouse.city}, {warehouse.country}
-        </div>
-        <div
-          className="warehouse-row__col warehouse-row__col--3"
-          data-label="Contact Name"
-        >
-          {warehouse.contact_name}
-        </div>
+	return (
+		<ul>
+			<li className="warehouse-row__row">
+				<div
+					className="warehouse-row__col warehouse-row__col--1"
+					data-label="warehouse item"
+				>
+					<Link
+						className="warehouse-row__col--title"
+						to={`/warehouses/${warehouse.id}`}
+					>
+						{warehouse.warehouse_name}
+					</Link>
+				</div>
+				<div
+					className="warehouse-row__col warehouse-row__col--2"
+					data-label="Address"
+				>
+					{warehouse.address}, {warehouse.city}, {warehouse.country}
+				</div>
+				<div
+					className="warehouse-row__col warehouse-row__col--3"
+					data-label="Contact Name"
+				>
+					{warehouse.contact_name}
+				</div>
 
-        <div
-          className="warehouse-row__col warehouse-row__col--4"
-          data-label="Contact Information"
-        >
-          <div>{warehouse.contact_phone}</div>
-          <div>{warehouse.contact_email}</div>
-        </div>
+				<div
+					className="warehouse-row__col warehouse-row__col--4"
+					data-label="Contact Information"
+				>
+					<div>{warehouse.contact_phone}</div>
+					<div>{warehouse.contact_email}</div>
+				</div>
 
-        <div
-          className="warehouse-row__col warehouse-row__col--5"
-          data-label="Actions"
-        >
-          <button
-            className="warehouse-row__col--btn"
-            onClick={() => setIsModalOpen(true)}
-          >
-            <img src={deleteImage} alt="delete" />
-          </button>
-          <Link
-            to={`/warehouse/${warehouse.id}/edit`}
-            className="warehouse-row__col--btn"
-          >
-            <img src={editImage} alt="edit" />
-          </Link>
-        </div>
-      </li>
+				<div
+					className="warehouse-row__col warehouse-row__col--5"
+					data-label="Actions"
+				>
+					<button
+						className="warehouse-row__col--btn"
+						onClick={() => setIsModalOpen(true)}
+					>
+						<img src={deleteImage} alt="delete" />
+					</button>
+					<Link
+						to={`/warehouses/${warehouse.id}/edit`}
+						className="warehouse-row__col--btn"
+					>
+						<img src={editImage} alt="edit" />
+					</Link>
+				</div>
+			</li>
 
-      <DeleteModal
-        isOpen={isModalOpen}
-        onClose={handleCloseModal}
-        itemType="warehouse"
-        itemId={warehouse.id}
-        itemName={warehouse.warehouse_name}
-        onSuccess={handleDeleteSuccess}
-      />
-    </ul>
-  );
+			<DeleteModal
+				isOpen={isModalOpen}
+				onClose={handleCloseModal}
+				itemType="warehouse"
+				itemId={warehouse.id}
+				itemName={warehouse.warehouse_name}
+				onSuccess={handleDeleteSuccess}
+			/>
+		</ul>
+	);
 }
 
 export default WarehouseRow;
