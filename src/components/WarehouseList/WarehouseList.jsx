9259c6c30dbc40e93b80ee25c50386cd4649be9e
nicolette-cheng <nikocheng@gmail.com>
2024-11-15 10:15:01 -0500

    copy and refactor InventoryList.jsx and InventoryList.scss for WarehouseList component

---
+++ src/components/WarehouseList/WarehouseList.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import "./WarehouseList.scss";
+
+import WarehouseRow from "../WarehouseRow/WarehouseRow.jsx";
+import WarehouseRibbon from "../WarehouseRibbon/WarehouseRibbon.jsx";
+import SearchHeader from "../SearchHeader/SearchHeader.jsx";
+
+import { useState } from "react";
+
+export default function WarehouseList({ warehouseItems }) {
+  const [warehouse, setWarehouse] = useState();
+
+  const [search, setSearch] = useState("");
+
+  const handleSearchInput = (event) => {
+    const { value } = event.target;
+    setSearch(value);
+  };
+
+  return (
+    <>
+      {/* Responsive Search Header */}
+      <SearchHeader
+        title="Warehouses"
+        buttonTitle="+ Add New Warehouse"
+        buttonLink="/warehouse/add"
+        handleSearchInput={handleSearchInput}
+      />
+      <div className="warehouse-list">
+        {/* The table header. Seperate from contents ribbon doesnt show on mobile */}
+        {/* Pass prop of what items to show in list for the warehouse inventory view */}
+        <WarehouseRibbon />
+        {/* Table contents */}
+        {warehouseItems.map((warehouse) => (
+          <WarehouseRow
+            key={warehouse.id}
+            warehouse={warehouse}
+            setWarehouse={setWarehouse}
+          />
+        ))}
+      </div>
+    </>
+  );
+}
