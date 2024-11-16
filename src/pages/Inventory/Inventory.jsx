3d06ac8989e8d58347b54fe5c85b6b2ed709c578
Ife Onuorah <ifeonuorah@gmail.com>
2024-11-16 18:38:23 -0400

    Remove console logs

--- src/pages/Inventory/Inventory.jsx
+++ src/pages/Inventory/Inventory.jsx
@@ -1,4 +1,3 @@
-import "./Inventory.scss";
 import InventoryList from "../../components/InventoryList/InventoryList";
 const apiUrl = import.meta.env.VITE_API_URL;
 
@@ -6,26 +5,26 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function Inventory() {
-  const [inventoryItems, setInventoryItems] = useState([]);
+	const [inventoryItems, setInventoryItems] = useState([]);
 
-  useEffect(() => {
-    generateInventoryItems();
-  }, []);
-  const generateInventoryItems = async () => {
-    try {
-      const { data } = await axios.get(`${apiUrl}/inventories`);
-      setInventoryItems(data);
-    } catch (error) {
-      console.error("Error fetching inventory data:", error);
-    }
-  };
+	useEffect(() => {
+		generateInventoryItems();
+	}, []);
+	const generateInventoryItems = async () => {
+		try {
+			const { data } = await axios.get(`${apiUrl}/inventories`);
+			setInventoryItems(data);
+		} catch (error) {
+			console.error("Error fetching inventory data:", error);
+		}
+	};
 
-  return (
-    <>
-      <InventoryList
-        inventoryItems={inventoryItems}
-        generateInventoryItems={generateInventoryItems}
-      />
-    </>
-  );
+	return (
+		<>
+			<InventoryList
+				inventoryItems={inventoryItems}
+				generateInventoryItems={generateInventoryItems}
+			/>
+		</>
+	);
 }
